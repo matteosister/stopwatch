b55ee3a64f12bef19ca4e9a173b1f77e2d3deb06
Matteo Giachino <matteog@gmail.com>
2016-01-13 00:42:15 +0100

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule StopWatch.Mixfile do
 
   def project do
     [app: :stopwatch,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      description: description,
      package: package,
